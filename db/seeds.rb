66751ec6cc4031df113f6eb2ca34004332c271e4
Kliti <hamatajkliti@gmail.com>
2021-03-17 13:45:21 +0100

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+url_ingredients = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+
+ingredients = JSON.parse(URI.parse(url_ingredients).open.string)
+
+puts 'Creating ingredients...'
+ingredients['drinks'].each do |ingredient|
+Ingredient.create!(name: ingredient['strIngredient1'])
+end
+
+puts 'Ingredients created'
